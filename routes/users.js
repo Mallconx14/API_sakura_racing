01b1339b343e2f854d0781f017fe8eb5493279ac
Mallconx14 <mallconx14@gmail.com>
2026-04-09 15:10:22 -0300

    rotas de post e get concluídas em USERS

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,29 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+//Get em users (read)
+router.get('/get', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        } else {
+            res.json(results);
+        }
+    });
+}); 
+
+//Post em usuários (create)
+router.post('/post', (req, res) => {
+    const { email, senha, nome } = req.body;
+    db.query('INSERT INTO users (email, senha, nome) VALUES (?, ?, ?)', [email, senha, nome], (err, results) => {
+        if (err) {
+            console.error("ERRO:", err);
+            res.status(500).json({ error: 'Erro ao inserir informações'});
+        } else {
+            res.json(results);
+        }   
+    });
+});
 
 
 module.exports = router;
