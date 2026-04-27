9bb5d91a595ea9a248442a322812e09605019ba3
Mallconx14 <mallconx14@gmail.com>
2026-04-27 08:30:43 -0300

    Criptografia adicionada às senhas

--- routes/corredores.js
+++ routes/corredores.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
 //Get em corredores (read)
 router.get('/get', (req, res) => {
@@ -14,9 +15,10 @@ router.get('/get', (req, res) => {
 }); 
 
 //Post em corredores (create)
-router.post('/post', (req, res) => {
+router.post('/post', async (req, res) => {
     const { nome, email, equipe, senha } = req.body;
-    db.query('INSERT INTO corredores (nome, email, equipe, senha) VALUES (?, ?, ?, ?)', [nome, email, equipe, senha], (err, results) => {
+    const senhaHash = await bcrypt.hashSync(senha, 10);
+    db.query('INSERT INTO corredores (nome, email, equipe, senha) VALUES (?, ?, ?, ?)', [nome, email, equipe, senhaHash], (err, results) => {
         if (err) {
             console.error("ERRO:", err);
             res.status(500).json({ error: 'Erro ao inserir informações'});
